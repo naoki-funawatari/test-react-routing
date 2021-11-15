0763c0cb582f408cc2de734335519c8aae80468a
naoki-funawatari <g011c1180@gmail.com>
2021-11-15 11:28:19 +0900

    feat: データ取得ロジックを各コンポーネントに移動。

--- src/routers/MainRouter.jsx
+++ src/routers/MainRouter.jsx
@@ -3,12 +3,9 @@ import { useParams } from "@/hooks/useParams";
 import Main from "@/components/Main";
 import Team from "@/features/racing/Team";
 import Driver from "@/features/racing/Driver";
-import teams from "@/assets/data/teams.json";
 
 const MainRouter = () => {
   const { teamId, driverId } = useParams();
-  const team = teams.find(o => o.teamId === Number(teamId));
-  const driver = team && team.drivers.find(o => o.driverId === Number(driverId));
 
   const router = useRoutes([
     {
@@ -17,11 +14,11 @@ const MainRouter = () => {
       children: [
         {
           path: ":team",
-          element: <Team {...{ team }} />,
+          element: <Team {...{ teamId }} />,
           children: [
             {
               path: ":driver",
-              element: <Driver {...{ driver }} />,
+              element: <Driver {...{ teamId, driverId }} />,
             },
           ],
         },
